0eb4cce3ac1fe69ab8f85b728fefc2b1368bef1b
Lois Appiah <appiahl2@vcu.edu>
2021-07-27 09:59:55 -0400

    add day, month, year view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/navigation-bar/nav-bar/nav-bar.component';
+import { MonthViewComponent } from './components/month-view/month-view/month-view.component';
+import { DayViewComponent } from './components/day-view/day-view/day-view.component';
+import { YearViewComponent } from './components/year-view/year-view/year-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MonthViewComponent,
+    DayViewComponent,
+    YearViewComponent
   ],
   imports: [
     BrowserModule,
