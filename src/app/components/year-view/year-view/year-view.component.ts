4901f1a3d1def05da36e1101e850f65d9998440e
Lois Appiah <appiahl2@vcu.edu>
2021-07-28 15:33:46 -0400

    create year display

--- src/app/components/year-view/year-view/year-view.component.ts
+++ src/app/components/year-view/year-view/year-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'app-year-view',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./year-view.component.css']
 })
 export class YearViewComponent implements OnInit {
+  year: number = 2021;
+  months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  displayMonth(mon: number) {
+    //add routing
+  }
+
+  // https://angular.io/start/start-routing
 }
