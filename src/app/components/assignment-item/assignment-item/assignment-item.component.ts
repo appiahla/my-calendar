6a567c637279de80ba121efb9f4f642516019144
Lois Appiah <appiahl2@vcu.edu>
2021-07-30 13:56:44 -0400

    create assignment-item

---
+++ src/app/components/assignment-item/assignment-item/assignment-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-item',
+  templateUrl: './assignment-item.component.html',
+  styleUrls: ['./assignment-item.component.css']
+})
+export class AssignmentItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
