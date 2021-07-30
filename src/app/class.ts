4491502e48e1b1eb40e95613ef66c6efee9aab88
Lois Appiah <appiahl2@vcu.edu>
2021-07-30 13:25:12 -0400

    add Class class

--- src/app/class.ts
+++ src/app/class.ts
@@ -1,7 +1,17 @@
+import { Assignment } from "./assignment";
+
 export class Class {
-  // color: string;
-  // name: string;
-  // professor: string;
-  // schedule: string[];
-  // assignments: any[];
+  color: string;
+  name: string;
+  professor: string;
+  schedule: string[];
+  assignments: Assignment[];
+
+  constructor(color: string, name: string, professor: string, schedule: string[], assignments: Assignment[]) {
+    this.color = color;
+    this.name = name;
+    this.professor = professor;
+    this.schedule = schedule;
+    this.assignments = assignments;
+  }
 }
