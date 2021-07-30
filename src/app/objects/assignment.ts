9d1c3bcde8aa67c0b55653e4b3ed089280a9df60
Lois Appiah <appiahl2@vcu.edu>
2021-07-30 14:08:44 -0400

    add testing assignments

--- src/app/objects/assignment.ts
+++ src/app/objects/assignment.ts
@@ -11,3 +11,18 @@ export class Assignment {
 }
 
 
+export const assignment1 = new Assignment(
+  "07/30/2021",
+  "Chapter 1 Notes",
+  [
+    "simple assignment",
+    "ask tabbi to work together"
+  ]  );
+
+export const assignment2 = new Assignment(
+  "08/30/2021",
+  "Chapter 5 Notes",
+  [
+    "hard assignment",
+    "ask tabbi to work together"
+  ]  );
